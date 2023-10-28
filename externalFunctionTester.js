67ff78aae911e74c5eaf93937a57239397ec855f
ReppinJesusChrist <cjw487@byu.edu>
2023-10-28 16:30:24 -0400

    Finished login code!!! It only works on the frontend, but it allows the menu to be rewritten in response to whether or not the user is signed in. This took a lot of work for something that doesn't look that impressive but I'm really happy with it.

--- externalFunctionTester.js
+++
@@ -1,7 +0,0 @@
-function test(){
-  // Create a new paragraph element
-  const paragraph = document.createElement('p');
-  paragraph.textContent = 'Set Goal button was pressed.';
-
-  document.body.appendChild(paragraph);
-}
---
+++ login.js
@@ -0,0 +1,24 @@
+function login() {
+  const emailEl     = document.querySelector("#email");
+  const passwordEl  = document.querySelector("#password");
+  const registerEl  = document.querySelector("#register");
+
+  localStorage.setItem("username",  emailEl.value);
+  localStorage.setItem("password", passwordEl.value);
+  localStorage.setItem("register", registerEl.checked);
+  sessionStorage.setItem("isLoggedIn", true);
+  //window.location.href = "goals.html";
+}
+
+function checkNavbarUnlock() {
+  const isLoggedIn = sessionStorage.getItem("isLoggedIn");
+  if(isLoggedIn){
+    const elem = document.querySelector("#security-navmenu");
+    console.log(elem);
+    elem.innerHTML =  '<li class="nav-item"> <a class="nav-link" href="index.html">Home</a></li>' +
+                        '<li class="nav-item"> <a class="nav-link" href="login.html">Login</a> </li>' +
+                        '<li class="nav-item"> <a class="nav-link" href="goals.html">Goals</a></li>' +
+                        '<li class="nav-item"> <a class="nav-link" href="accountability.html">Accountability</a> </li>' +
+                        '<li class="nav-item"> <a class="nav-link" href="social.html">Social</a></li>';
+  }
+}
