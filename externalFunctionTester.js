d3fb3b0348e0801dbf2618743c0cb4649cd6b0d5
ReppinJesusChrist <cjw487@byu.edu>
2023-10-28 13:10:30 -0400

    Update to match changes I made in git-bash

---
+++ externalFunctionTester.js
@@ -0,0 +1,7 @@
+function test(){
+  // Create a new paragraph element
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'Set Goal button was pressed.';
+
+  document.body.appendChild(paragraph);
+}
