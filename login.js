3b925d8cdd8daa87a11662b1c9a89f519e588510
ReppinJesusChrist <cjw487@byu.edu>
2023-10-28 20:04:50 -0400

    Username display is now functional on two pages. The rest still need to be debugged. It's going to be a long night.

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 function updatePage(){
   checkNavbarUnlock();
-
+  checkDisplayUsername();
 }
 
 function login() {
-  const usernameEl     = document.querySelector("#username");
+  const usernameEl  = document.querySelector("#username");
   const passwordEl  = document.querySelector("#password");
   const registerEl  = document.querySelector("#register");
 
@@ -20,15 +20,21 @@ function checkNavbarUnlock() {
   const isLoggedIn = sessionStorage.getItem("isLoggedIn");
   if(isLoggedIn){
     const elem = document.querySelector("#security-navmenu");
-    console.log(elem);
-    elem.innerHTML =  '<li class="nav-item"> <a class="nav-link" href="index.html">Home</a></li>' +
+    if(elem){
+      elem.innerHTML =  '<li class="nav-item"> <a class="nav-link" href="index.html">Home</a></li>' +
                         '<li class="nav-item"> <a class="nav-link" href="login.html">Login</a> </li>' +
                         '<li class="nav-item"> <a class="nav-link" href="goals.html">Goals</a></li>' +
                         '<li class="nav-item"> <a class="nav-link" href="accountability.html">Accountability</a> </li>' +
                         '<li class="nav-item"> <a class="nav-link" href="social.html">Social</a></li>';
+    }
   }
 }
 
-function displayUsername() {
-  
+function checkDisplayUsername() {
+  const displayName = localStorage.getItem("username");
+  console.log(localStorage.getItem(username));
+  if(displayName){
+    const headerEl = document.querySelector(".navbar-brand");
+    headerEl.innerHTML = '<strong>Covenant Companion</strong> - Currently Logged-in as: ' + displayName;
+  }
 }
