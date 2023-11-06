d7ca9fc3cd3627413298e9b7a46e79a1a1d49c62
ReppinJesusChrist <cjw487@byu.edu>
2023-11-06 16:55:11 -0500

    Adds lots of files. I really hope this works

--- login.js
+++ login.js
@@ -23,6 +23,7 @@ function checkNavbarUnlock() {
     const elem = document.querySelector("#security-navmenu");
     if(elem){
       elem.innerHTML =  '<li class="nav-item"> <a class="nav-link" href="index.html">Home</a></li>' +
+                        '<li class="nav-item"><a class="nav-link" href="info.html">Info</a></li>' +
                         '<li class="nav-item"> <a class="nav-link" href="login.html">Login</a> </li>' +
                         '<li class="nav-item"> <a class="nav-link" href="goals.html">Goals</a></li>' +
                         '<li class="nav-item"> <a class="nav-link" href="accountability.html">Accountability</a> </li>' +
